dc94820faec8411c05b471e07fcd0e71fa78d4f3
miguel-bermell <miguelbermell@gmail.com>
2025-03-26 04:06:56 +0100

    feat(): add page lang

--- src/lib/utils/getPageProps.ts
+++ src/lib/utils/getPageProps.ts
@@ -1,7 +1,9 @@
+import type { Languages } from "../../types/Languages";
 import getCommonData from "./getCommonData";
 
 export const getPageProps = async (Astro: any) => {
   const isPost = Astro.request?.method === "POST";
-  const props = isPost ? await Astro.request.json() : await getCommonData(Astro.url.pathname, 'es')
+  const language = Astro.url.searchParams.get('language') ?? 'es' as Languages;
+  const props = isPost ? await Astro.request.json() : await getCommonData(Astro.url.pathname, language);
   return { props };
 }
--- src/pages/api/generate-pdf/[page].ts
+++ src/pages/api/generate-pdf/[page].ts
@@ -1,13 +1,23 @@
 import type { APIRoute } from "astro";
 import { sendToQueue } from "../../../lib/amqp";
+import { PAGES } from "../../../lib/enums/pages";
+import type { Languages } from "../../../types/Languages";
+
 
 export const prerender = false;
 
 export const POST: APIRoute = async ({ params, request }) => {
+  const query = new URLSearchParams(request.url);
+
   const { page } = params;
   const body = await request.json();
 
-  const url = `http://ms-pdf:4321/${page}`;
+  if (!page || !Object.values(PAGES).includes(page)) {
+    return new Response("Invalid page", { status: 400 });
+  }
+
+  const language = query.get("language") as Languages ?? "es";
+  const url = `http://ms-pdf:4321/${page}?language=${language}`;
 
   await sendToQueue(url, body);
   
